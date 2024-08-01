fb3570e6fec303d8c350df91073b7105a60aedc8
thanhdangg <102210310@sv1.dut.udn.vn>
2024-08-02 00:36:54 +0700

    add image

--- lib/ImageWithBoxes.dart
+++ lib/ImageWithBoxes.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:yolo_app/DetectionResult.dart';
@@ -10,37 +11,72 @@ class ImageWithBoxes extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Image.file(File(imagePath)),
-        ...results.map((result) {
-          return Positioned(
-            left: result.xmin,
-            top: result.ymin,
-            width: result.xmax - result.xmin,
-            height: result.ymax - result.ymin,
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.red,
-                  width: 2,
-                ),
-              ),
-              child: Align(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  '${result.name} ${(result.confidence * 100).toStringAsFixed(1)}%',
-                  style: const TextStyle(
-                    backgroundColor: Colors.red,
-                    color: Colors.white,
-                    fontSize: 12,
-                  ),
-                ),
-              ),
-            ),
-          );
-        }).toList(),
-      ],
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final image = Image.file(File(imagePath));
+        return FutureBuilder<Size>(
+          future: _getImageSize(image),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(child: CircularProgressIndicator());
+            }
+
+            final imageSize = snapshot.data!;
+            final scaleX = constraints.maxWidth / imageSize.width;
+            final scaleY = constraints.maxHeight / imageSize.height;
+
+            return Stack(
+              fit: StackFit.expand,
+              children: [
+                image,
+                ...results.map((result) {
+                  final left = result.xmin * scaleX;
+                  final top = result.ymin * scaleY;
+                  final width = (result.xmax - result.xmin) * scaleX;
+                  final height = (result.ymax - result.ymin) * scaleY;
+
+                  return Positioned(
+                    left: left,
+                    top: top,
+                    width: width,
+                    height: height,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.red,
+                          width: 2,
+                        ),
+                      ),
+                      child: Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          '${result.name} ${(result.confidence * 100).toStringAsFixed(1)}%',
+                          style: const TextStyle(
+                            backgroundColor: Colors.red,
+                            color: Colors.white,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ],
+            );
+          },
+        );
+      },
     );
+  }
+
+  Future<Size> _getImageSize(Image image) {
+    final completer = Completer<Size>();
+    image.image.resolve(ImageConfiguration()).addListener(
+      ImageStreamListener((ImageInfo info, bool _) {
+        completer.complete(
+            Size(info.image.width.toDouble(), info.image.height.toDouble()));
+      }),
+    );
+    return completer.future;
   }
 }
